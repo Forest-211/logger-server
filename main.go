4b8643d4f01c69c9fa869c686f6101e73bf25f18
changlin <changlin@SEAGMdeMac-mini.RT-AX88U>
2021-09-30 17:09:05 +0800

    feat: add logger server

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"server/middleware"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var html = template.Must(template.New("https").Parse(`
+<html>
+<head>
+  <title>title</title>
+  <script src="/assets/app.js"></script>
+</head>
+<body>
+  <h1 style="color:red;">Welcome, Ginner!</h1>
+</body>
+</html>
+`))
+
+func main() {
+	router := gin.Default()
+	router.Static("/assets", "./assets")
+	router.SetHTMLTemplate(html)
+	router.Use(middleware.Logger())
+
+	router.GET("/html", func(c *gin.Context) {
+		if pusher := c.Writer.Pusher(); pusher != nil {
+			// 使用 pusher.Push() 做服务器推送
+			if err := pusher.Push("/assets/app.js", nil); err != nil {
+				log.Printf("Failed to push: %v", err)
+			}
+		}
+		c.HTML(200, "https", gin.H{
+			"status": "success",
+		})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		// 服务连接
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+}
---
+++ middleware/logger.go
@@ -0,0 +1,123 @@
+package middleware
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	retalog "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
+)
+
+func Logger() gin.HandlerFunc {
+	// 定义文件路径
+	filePath := "log/server"
+	// 软连接路径(可以根据运维来进行调整)
+	// linkName := "latest_log.log"
+	// 初始化
+	logger := logrus.New()
+	// 定位到log文件夹(路径，os新建文件，赋值权限)
+	src, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 输出到日志文件中
+	logger.Out = src
+
+	logger.SetLevel(logrus.DebugLevel)
+	logWriter, _ := retalog.New(
+		filePath+"_%Y-%m-%d.json",
+		retalog.WithMaxAge(7*24*time.Hour),
+		retalog.WithRotationTime(24*time.Hour),
+		// retalog.WithLinkName(linkName),
+	)
+	// 日志级别
+	writeMap := lfshook.WriterMap{
+		logrus.InfoLevel:  logWriter,
+		logrus.FatalLevel: logWriter,
+		logrus.DebugLevel: logWriter,
+		logrus.WarnLevel:  logWriter,
+		logrus.ErrorLevel: logWriter,
+		logrus.PanicLevel: logWriter,
+	}
+	// 时间格式化分割
+	Hook := lfshook.NewHook(writeMap, &logrus.JSONFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		PrettyPrint:     true,
+	})
+
+	logger.AddHook(Hook)
+
+	return func(c *gin.Context) {
+		// 输出开始时间
+		startTime := time.Now()
+
+		c.Next()
+
+		// 结束时间
+		stopTime := time.Since(startTime)
+
+		// 执行时间
+		latencyTime := fmt.Sprintf("%d ms", int(math.Ceil(float64(stopTime.Nanoseconds())/1000000.0)))
+
+		// 主机名
+		hostName, err := os.Hostname()
+		if err != nil {
+			hostName = "unKnown"
+		}
+
+		// 状态码
+		statusCode := c.Writer.Status()
+
+		// 客户端ip
+		clientIP := c.ClientIP()
+
+		// 客户端信息
+		userAgent := c.Request.UserAgent()
+
+		// 请求大小
+		dataSize := c.Writer.Size()
+		if dataSize < 0 {
+			dataSize = 0
+		}
+
+		// 请求方法
+		method := c.Request.Method
+
+		// 请求路径
+		path := c.Request.RequestURI
+
+		// 请求参数
+		params := c.Request.Body
+
+		// 组合数据
+		entry := logger.WithFields(logrus.Fields{
+			"host-name":    hostName,
+			"status":       statusCode,
+			"latency-time": latencyTime,
+			"ip":           clientIP,
+			"method":       method,
+			"path":         path,
+			"data-size":    dataSize,
+			"agent":        userAgent,
+			"end-time":     stopTime,
+			"params":       params,
+		})
+
+		// 分级别
+		if len(c.Errors) > 0 {
+			entry.Error(c.Errors.ByType(gin.ErrorTypePrivate).String())
+		}
+
+		if statusCode >= 500 {
+			entry.Error()
+		} else if statusCode >= 400 {
+			entry.Warn()
+		} else {
+			entry.Info()
+		}
+	}
+}
